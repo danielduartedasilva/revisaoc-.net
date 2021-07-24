8840146dd1901e14a061502cd26315d1d8a14e6c
Daniel Duarte <danielddsilva10@gmail.com>
2021-07-24 20:22:21 -0300

    Cadastro simples

---
+++ VendasConsole/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendasConsole
+{
+    class Cliente
+    {
+        //atributos, propriedades e característica
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VendasConsole
 {
@@ -6,7 +7,45 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            Cliente c = new Cliente();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-- Cadastrar Cliente --");
+                Console.WriteLine("1 - Digite o nome do Cliente");
+                Console.WriteLine("2 - Digite o cpf do Cliente");
+                Console.WriteLine("3 - Listar Cliente");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("\nDigite a opção desejada:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("1 - Cadastrar Cliente");
+                        Console.WriteLine("2 - Digite o nome do Cliente");
+                        c.Nome = Console.ReadLine();
+                        Console.WriteLine("2 - Digite o cpf do Cliente");
+                        c.Cpf = Console.ReadLine();
+                        //Mensagem de sucesso
+                        Console.WriteLine($"Nome: {c.Nome} e Cpf: {c.Cpf}");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nListar Cliente");
+                        //Laço de repetição para mostrar todos os clientes
+                        break;
+                    case 0:
+                        Console.WriteLine("\nSair");
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção inválida");
+                        break;
+                }
+                Console.WriteLine("\nPressione uma tecla para continuar:");
+                Console.ReadKey();
+            } while (opcao != 0);
+
         }
     }
 }
